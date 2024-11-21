82120f3328434e648fcd67a68394234a65969d0d
JoePlato <guns.m.lyon@gmail.com>
2024-11-21 14:25:44 -0500

    Finished up Part 2

--- js/part2.js
+++ js/part2.js
@@ -7,4 +7,19 @@ document.addEventListener("DOMContentLoaded", async () => {
     // The format should follow the example user in the HTML file.
 
     // TODO
+    const exampleCard = document.querySelector(".card");
+    const cardList = document.getElementById("user-list");
+    let params = new URLSearchParams( {page: 1} );
+    const resp = await fetch("https://reqres.in/api/users?" + params.toString());
+    const echo = await resp.json();
+    //console.log(echo.data);
+    Object.entries(echo.data).forEach(([key, value]) => {
+        let clonedCard = exampleCard.cloneNode(true);
+        const children = clonedCard.children;
+        clonedCard.id = toString(key);
+        children[0].textContent = value.first_name + " " +value.last_name;
+        children[1].src = value.avatar
+        cardList.appendChild(clonedCard);
+
+    })
 });

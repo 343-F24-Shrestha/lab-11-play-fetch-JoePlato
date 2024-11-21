84ed54c5efc345af0df2ebd0539142b76ba89686
JoePlato <guns.m.lyon@gmail.com>
2024-11-21 14:39:39 -0500

    Finished part 3

--- js/part3.js
+++ js/part3.js
@@ -6,11 +6,16 @@ document.getElementById('api-1-btn').addEventListener('click', async () => {
     // If your response has no body, put the status code in output-1.
 
     // TODO
+    const response = await fetch("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+    const json  = await response.json();
+    output1.textContent = JSON.stringify(json, null, 2);
 });
 
 document.getElementById('api-2-btn').addEventListener('click', async () => {
     // Make a request to your second API here. Put the response's data in output-2.
     // If your response has no body, put the status code in output-2.
-    
+    const response = await fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json&page=2");
+    const json  = await response.json();
+    output2.textContent = JSON.stringify(json, null, 2);
     // TODO
 });
